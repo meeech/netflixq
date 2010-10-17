f2f5149984ce1a64bce1b60c8fb0c9ec6f622c6f
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-10-17 04:05:54 -0400

    Progress has been mase

--- pagescript.js
+++ pagescript.js
@@ -1,10 +1,44 @@
 Y.use('node', function(Y) {
 
-    var button = '<div class="netflixq btn btn-40 watchlk">netflixq</div>';
+    var selectors = {
+        PLAY_WRAPPER: '.btnWrap',
+        BUTTONS_CONTAINER: '.agMovieSet',
+        BUTTON_NETFLIXQ: '.netflixq'
+    };
+
+    var button = '<div class="netflixq btn btn-30 watchlk">netflixq</div>';
 
     var init = function() {
-        
-        Y.all('.btnWrap').insert(button);
+
+        Y.all(selectors.PLAY_WRAPPER).insert(button);
+
+        //Delegate to catch all netflixq clicks
+        Y.one(selectors.BUTTONS_CONTAINER).delegate('click', function(e) {
+            //e.currentTarget -  defined target element - one we looking for
+            //e.target - The element that was really clicked
+            //e.container - the node we delegated on
+            //Figure out what was clicked. 
+            
+            // Movie Grid
+            // agMovieSet agMovieGrid agMovieGridCol5
+            //div.agMovie
+            //Movie Table
+            //agMovieSet agMovieTable
+            // tr.agMovie
+            var row = e.target.ancestor('.agMovie');
+            var link = row.one('.title a');
+            var movie = {
+                id: link.get('id'),
+                title: link.get('innerText'),
+                url: link.get('href')
+            };
+            
+            
+            window.console.log(link);
+            window.console.log(movie);
+            
+            //Post message 
+        }, selectors.BUTTON_NETFLIXQ);
         
         //Y.all('.btnWrap').setContent('test!');
         // window.console.log(Y.all('.btnWrap'));
