6b16e9d4e5bc275155d94b0429d6693658db0f3c
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-10-17 16:00:05 -0400

    Ok. The three main views are working.

--- pagescript.js
+++ pagescript.js
@@ -1,69 +1,62 @@
 Y.use('node', function(Y) {
 
-    var selectors = {
-        PLAY_WRAPPER: '.btnWrap',
-        BUTTONS_CONTAINER: '.main-content',
-        // MOVIE_VIEW_ID:         
-        BUTTON_NETFLIXQ: '.netflixq'
-        
-    };
+var selectors = {
+    PLAY_WRAPPER: '.btnWrap',
+    BUTTONS_CONTAINER: '.main-content',
+    BUTTON_NETFLIXQ: '.netflixq'
+};
 
-    var button = '<a class="netflixq btn {btn-size} watchlk">netflixq</a>';
+var button = '<a class="netflixq btn {btn-size} watchlk">netflixq</a>';
 
-    // netflix creates some ids like m70102568_0, m70102568_1 and so forth for different elements
-    // in the page for a particular movie. use this to extract the uuid part.
-    var extractid = function(id) {
-        return id.slice(0, id.lastIndexOf('_'));        
-    };
+// netflix creates some ids like m70102568_0, m70102568_1 and so forth for different elements
+// in the page for a particular movie. use this to extract the uuid part.
+var extractid = function(id) {
+    return id.slice(0, id.lastIndexOf('_'));        
+};
 
-    var init = function() {
+var init = function() {
+    //Add in the netflixQ button
+    Y.all(selectors.PLAY_WRAPPER).filter(':not(.mltBtn-trailer)').insert(button);
 
-        Y.all(selectors.PLAY_WRAPPER).filter(':not(.mltBtn-trailer)').insert(button);
-// window.console.log(Y.one(selectors.BUTTONS_CONTAINER));
-        //Delegate to catch all netflixq clicks
-        //Movie Grid
-        //agMovieSet agMovieGrid agMovieGridCol5
-        //div.agMovie
+    Y.one(selectors.BUTTONS_CONTAINER).delegate('click', function(e) {
 
-        //Movie Table
-        //agMovieSet agMovieTable
-        //tr.agMovie
-        Y.one(selectors.BUTTONS_CONTAINER).delegate('click', function(e) {
+        //@todo clone and animate to show click.
+        var row = e.target.ancestor('.agMovie');
 
-            //@todo clone and animate to show click.
-            var row = e.target.ancestor('.agMovie');
+        //Will use titleEl as an indicator of which page we on
+        //If it come back null, we know we on one of the listing pages. 
+        //otherwise, we on a moview view page
+        var titleEl = Y.one('h2.title'),
+            link,
+            title,
+            url;
 
-            //Will use this as an indicator of which page we on
-            //If it come back null, we know we on one of the listing pages. 
-            //otherwise, we on a moview view page
-            var titleEl = Y.one('h2.title'),
-                link, 
-                title;
+        //Listing, Grid, page
+        if( Y.Lang.isNull(titleEl) ) {
+            link = row.one('.title a');
+            title = link.get('innerText');
+            url = link.get('href');
+        } 
+        else { //Movie View Page
+            link = row.one('a.btn-play');
+            title = titleEl.get('innerText');
+            url = window.location.href;
+        }
 
-            if( Y.Lang.isNull(titleEl) ) {
-                link = row.one('.title a');
-                title = link.get('innerText');
-            } else {
-                link = row.one('a.btn-play') ;
-                title = titleEl.get('innerText') ;
-            }
+        var movie = {
+            "id": extractid(link.get('id')),
+            "title": title,
+            "url": url
+        };
 
-            // var url = ( Y.Lang.isNull(titleEl) )
-            var movie = {
-                id: extractid(link.get('id')),
-                title: title,
-                url: link.get('href')
-            };
+        chrome.extension.sendRequest(movie, function(response) {
+            //Disable/remove the button?.
+            // window.console.log(response);
+        });
 
-            chrome.extension.sendRequest(movie, function(response) {
-                //Disable/remove the button?.
-                // window.console.log(response);
-            });
+    }, selectors.BUTTON_NETFLIXQ);
+};
 
-        }, selectors.BUTTON_NETFLIXQ);
-    };
-
-    init();
+init();
     
 });
-
