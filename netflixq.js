aacf41249beb88f465c752a4b0a5324897c6252e
mitchell amihod <mitchell@amihod.com>
2013-03-28 10:58:19 -0300

    remove some cruft.

--- netflixq.js
+++ netflixq.js
@@ -50,40 +50,4 @@ Y.one(selectors.Q_LIST).delegate('click', function(e) {
 }, selectors.REMOVE_TARGET);
 
 
-// Y.one(selectors.Q_LIST).delegate('mouseover', function(e) {
-//     showTooltip(e.target);
-// }, 'a');
-// 
-// Y.one(selectors.Q_LIST).delegate('mouseout', function(e) {
-//    hideTooltip(e.target);
-// }, 'a');
-
-
-// var showTooltip = function(node) {
-//     if(29 > node.get('innerHTML').length) {
-//         return;
-//     }
-// 
-//     var tt = node.getData('tt');
-// 
-//     if(!tt) {
-//         tt = new Y.Overlay({
-//             bodyContent: node.get('innerHTML'),
-//             align: { node: '#container', points: [Y.WidgetPositionAlign.BC, Y.WidgetPositionAlign.BC] }
-//         });
-//         tt.hide();
-//         tt.render(selectors.Q_LIST);
-//         node.setData('tt', tt);    
-//     }
-//     
-//     tt.show();
-// };
-// 
-// var hideTooltip = function(node) {
-//     var tt = node.getData('tt');
-//     if(tt) {
-//         tt.hide();
-//     }
-// };
-
 });
